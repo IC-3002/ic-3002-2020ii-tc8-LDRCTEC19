2e25152847ae6b06f060aacb33325580b8e6f196
LDRCTEC19 <ldrc161997@gmail.com>
2020-11-25 18:20:30 -0600

    Tarea 8

--- rutas.py
+++ rutas.py
@@ -1,2 +1,12 @@
 def contar_rutas_mas_cortas(C):
-    raise NotImplementedError()
+    m = auxiliar(C,1,2)
+    if m==[]:
+        return []
+    else: 
+        for i in range(0, len(m)):
+            for j in range (0, len(m[0])):
+                if matriz[i][j] == 'T':
+                    matriz[i][j] = 1
+                else:
+                    matriz[i][j] = 0
+    return matriz
